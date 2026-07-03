d33ddece75bbbd463973f7587e36945c74fb8886
Aniket Ramteke <aramteke870@gmail.com>
2026-07-03 17:32:17 +0530

    This is Diff commit

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-name = input("Enter your name: ")
-age = int(input("Enter your age: "))
+name = input("Enter your name: Please")
+age = int(input("Enter your age: Please"))
 
 print(f"Hello, {name}!")
 print(f"You are {age} years old.")
